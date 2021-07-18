85f075ed2c2352e4d8b92aa0e57d8ea7cddec459
jeromeski <jeromegee@gmail.com>
2021-07-18 12:20:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-vrug2w
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-vrug2w)
---
+++ app.js
@@ -0,0 +1,18 @@
+console.log(); // global
+
+// global == window
+// browsers have window {}
+// node has global {}
+// but node.js uses global
+
+//  Exs of methods that are part of the standard set of methods that can be found inside the global {}
+// 1. setTimeout()
+// 2. clearTimeout()
+// 3. setInterval()
+// 4. clearInterval()
+
+var message = 'Hello World';
+console.log(global.message);
+// output: undefined
+
+// *variables and functions are scoped only to this index.js file.
